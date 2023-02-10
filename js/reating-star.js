24f07333eb5082e7214d1e3151a2e50d89affdad
AmurKhoyetsyan <javanewss@gmail.com>
2023-02-10 22:42:34 +0400

    replace js

--- js/reating-star.js
+++ js/reating-star.js
@@ -95,9 +95,9 @@
 })();
 
 ;(function(){
-    const StarReating = function (parent, callback = null) {
+    const StarReating = function (parent, option) {
         this.parent = parent;
-        this.callback = callback;
+        this.option = option;
     };
 
     StarReating.prototype.createStyle = function () {
@@ -106,7 +106,7 @@
         if (head && !style) {
             let styles = document.createElement('style');
             styles.setAttribute('type', 'text/css');
-            styles.innerText = ".arm-star-reting-content-star,.arm-star-reting-content-star *{-webkit-box-sizing:border-box;-moz-box-sizing:border-box;box-sizing:border-box;margin:0;padding:0}.arm-star-reting-content-star{width:100%;max-width:100px;height:20px}.arm-star-reting-content-star .arm-star-reting-waveform-bg{-webkit-clip-path:url(#buckets);clip-path:url(#buckets);fill:#FFFFFF}.arm-star-reting-waveform-bg{cursor:pointer}.arm-star-reting-mouse-over.overed{cursor:pointer;fill:rgb(255,200,0)}";
+            styles.innerText = ".arm-star-reting-content-star,.arm-star-reting-content-star *{-webkit-box-sizing:border-box;-moz-box-sizing:border-box;box-sizing:border-box;margin:0;padding:0}.arm-star-reting-content-star{width:100%;max-width:100px;height:20px}.arm-star-reting-mouse-over{cursor:pointer;}.arm-star-reting-mouse-over.arm-star-overed{cursor:pointer;fill:rgb(255,200,0)}";
             head[0].appendChild(styles);
         }
     };
@@ -157,21 +157,23 @@
         rectTop.setAttribute('width', '100%');
         rectTop.setAttribute('x', 0);
         rectTop.setAttribute('y', 0);
-        rectTop.setAttribute('class', 'arm-star-reting-waveform-bg');
+        rectTop.setAttribute('class', `arm-star-reting-waveform-bg-${index}`);
+
+        let percent = item.hasAttribute('arm-star-data-percent') ? parseFloat(item.getAttribute('arm-star-data-percent')) : 0;
 
         let rectBottom = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
         rectBottom.setAttribute('height', 20);
-        rectBottom.setAttribute('width', '100%');
+        rectBottom.setAttribute('width', `${percent}%`);
         rectBottom.setAttribute('x', 0);
         rectBottom.setAttribute('y', 0);
-        rectBottom.setAttribute('class', `.arm-star-reting-progress-star-${index}`);
+        rectBottom.setAttribute('class', `arm-star-reting-progress-star-${index}`);
         rectBottom.setAttribute('fill', 'rgb(255,200,0)');
 
         let svgTop = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-        svgTop.setAttribute('height', 20);
-        svgTop.setAttribute('width', 100);
-        svgTop.setAttribute('preserveaspectratio', 'none');
         svgTop.setAttribute('viewBox', '0 0 100 20');
+        svgTop.setAttribute('height', 20);
+        svgTop.setAttribute('width', '100%');
+        svgTop.setAttribute('preserveaspectratio', 'none');
         svgTop.setAttribute('xmlns','http://www.w3.org/2000/svg');
         svgTop.setAttribute('xmlns:xlink','http://www.w3.org/1999/xlink');
         svgTop.setAttribute('class', 'arm-star-reting-waveform-container');
@@ -180,23 +182,23 @@
         svgTop.appendChild(rectBottom);
 
         let contentStar = document.createElement('div');
-        contentStar.classList.add(`arm-star-reting-content-star-${index}`);
+        contentStar.classList.add(`arm-star-reting-content-star`);
 
         let styles = document.createElement('style');
         styles.setAttribute('type', 'text/css');
-        styles.innerText = `.arm-star-reting-content-star-${index} .arm-star-reting-progress-star-${index} { -webkit-clip-path: url(#buckets${index}); clip-path: url(#buckets${index}); }`
+        styles.innerText = `.arm-star-reting-content-star .arm-star-reting-waveform-bg-${index} {-webkit-clip-path:url(#buckets${index});clip-path:url(#buckets${index});fill:#FFFFFF} .arm-star-reting-content-star .arm-star-reting-progress-star-${index} { -webkit-clip-path: url(#buckets${index}); clip-path: url(#buckets${index}); }`
 
         let svgBottom = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
         svgBottom.setAttribute('height', 0);
         svgBottom.setAttribute('width', 0);
         svgBottom.setAttribute('xmlns','http://www.w3.org/2000/svg');
         svgBottom.setAttribute('xmlns:xlink','http://www.w3.org/1999/xlink');
-        svgBottom.setAttribute('class', 'arm-star-reting-waveform-container');
+        svgBottom.setAttribute('class', 'arm-star-reting-mask');
 
         let defs = document.createElementNS('http://www.w3.org/2000/svg', 'defs');
         let clipPath = document.createElementNS('http://www.w3.org/2000/svg', 'clipPath');
 
-        clipPath.setAttribute('id', `#buckets${index}`);
+        clipPath.setAttribute('id', `buckets${index}`);
 
         let count = 5;
         let space = 4;
@@ -209,21 +211,21 @@
             buckets.push(i);
         }
 
-        // mask.innerHTML = buckets.map((bucket, index) => {
-        //     let positionX = index === 0 ? (index + 0.5) * 20 : index * 20;
-        //     let starPath = star((index + 0.5) * 20, 10, 10, 5);
-        //     return `<path d=${starPath} fill="rgba(255,200,0, 1)" />`;
-        // }).join('');
+        let mask = '';
 
-        let content = '';
-
-        content = content + buckets.map((bucket, index) => {
+        mask = mask + buckets.map((bucket, index) => {
             let positionX = index === 0 ? (index + 0.5) * 20 : index * 20;
             let starPath = this.star((index + 0.5) * 20, 10, 10, 5);
-            return `<path d=${starPath} fill="none" stroke="rgba(0,0,0,1)" class="mouse-over" strokeWidth="2" data-index='${index}' />`;
+            return `<path d=${starPath} fill="rgba(255,200,0, 1)" />`;
         }).join('');
 
-        clipPath.innerHTML = content;
+        clipPath.innerHTML = mask;
+
+        svgTop.innerHTML = svgTop.innerHTML + buckets.map((bucket, index) => {
+            let positionX = index === 0 ? (index + 0.5) * 20 : index * 20;
+            let starPath = this.star((index + 0.5) * 20, 10, 10, 5);
+            return `<path d=${starPath} fill="none" stroke="rgba(0,0,0,1)" class="arm-star-reting-mouse-over" strokeWidth="2" data-index='${index}' />`;
+        }).join('');
 
         defs.appendChild(clipPath);
         svgBottom.appendChild(defs);
@@ -240,6 +242,48 @@
         if (parents) {
             parents.forEach((item, index) => {
                 this.createSvg(item, index);
+
+                let mouseOver = item.querySelectorAll('.arm-star-reting-mouse-over');
+                let cursorPoint = item.querySelector('.arm-star-reting-content-star');
+
+                cursorPoint.onmouseover = function (event) {
+                    let child = item.querySelector('.arm-star-reting-mask');
+                    let rect = item.querySelector(`.arm-star-reting-progress-star-${index}`);
+
+                    child.style['opacity'] = '0';
+                    rect.style['opacity'] = '0';
+                }
+
+                cursorPoint.onmouseout = function (event) {
+                    let child = item.querySelector('.arm-star-reting-mask');
+                    let rect = item.querySelector(`.arm-star-reting-progress-star-${index}`);
+                    setTimeout(() => {
+                        child.style['opacity'] = '1';
+                        rect.style['opacity'] = '1';
+                    }, 100);
+                }
+
+                mouseOver.forEach((starItem, starIndex) => {
+                    starItem.onmouseover = function (event) {
+                        for (let i = 0; i <= starIndex; i++) {
+                            if (!mouseOver.item(i).classList.contains('arm-star-overed')) {
+                                mouseOver.item(i).classList.add('arm-star-overed');
+                            }
+                        }
+                    }
+
+                    starItem.onmouseout = function (event) {
+                        for (let i = 0; i <= starIndex; i++) {
+                            if (mouseOver.item(i).classList.contains('arm-star-overed')) {
+                                mouseOver.item(i).classList.remove('arm-star-overed');
+                            }
+                        }
+                    }
+
+                    starItem.onclick = function (event) {
+                        console.log(index + 1);
+                    }
+                });
             });
         }
     };
@@ -249,8 +293,10 @@
         this.createStar();
     };
 
-    let star = new StarReating('.fff', function(json) {
-        console.log('clicked', json)
+    let star = new StarReating('.star-bar', {
+        onclick: function(json) {
+            console.log('clicked', json)
+        }
     });
     star.run();
 })();
